9551ffac55925d1d64bd054fc81540f2bc4d8a8f
COUPLE-ELI <couple1999elijah@gmail.com>
2023-09-11 15:27:24 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,3 +1,16 @@
 #include <stdio.h>
 /**
- * main -
+ * main - function
+ *
+ * Return: successful
+ */
+
+int main(void)
+{
+	printf("size of a char: %lu byte(s)", sizeof(char));
+	printf("size of int: %lu byte(s)", sizeof(int));
+	printf("size of a long int: %lu bytes(s)", sizeof(long int));
+	printf("size of a long long int: %lu bytes(s)", sizeof(long long int));
+	printf("size of float: %lu bytes(s)", sizeof(float));
+	return (0);
+}
